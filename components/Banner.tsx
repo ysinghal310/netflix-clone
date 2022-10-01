c4a5c3cd2c747915183f0f8df342592b2680801d
Yash Singhal <75132721+codexgang@users.noreply.github.com>
2022-10-01 20:40:43 +0530

    working on banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,13 +1,33 @@
 import Image from 'next/image'
+import { Movie } from '../typing';
+import { useState, useEffect }  from "react"
+import { baseUrl } from '../constant/movie';
 
-const Banner = () => {
+interface Props {
+  netflixOriginals: Movie[],
+}
+
+const Banner = ({netflixOriginals}: Props) => {
+  const [movie,setMovie] = useState<Movie | null>(null);
+
+
+  useEffect(()=>{
+    setMovie(netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)])
+  },[netflixOriginals])
+
   return (
     <div>
-        {/* <div>
-            <Image />
-        </div> */}
+        <div className='absolute -z-10 top-0 left-0 h-[95vh] w-screen'>
+          <Image 
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          layout="fill"
+          objectFit='cover'
+          />
+        
+        </div>
     </div>
   )
 }
 
-export default Banner
+export default Banner;
+
