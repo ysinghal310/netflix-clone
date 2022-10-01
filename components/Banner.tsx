3735de6500c18d25c4a1473838052c7b98c0eea0
Yash Singhal <75132721+codexgang@users.noreply.github.com>
2022-10-01 15:35:45 +0530

    first commit

---
+++ components/Banner.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+const Banner = () => {
+  return (
+    <div>
+        {/* <div>
+            <Image />
+        </div> */}
+    </div>
+  )
+}
+
+export default Banner
